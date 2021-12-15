903c1f5d37199d1d7ca4938b3601d125b2ad4e7f
Ian Morland <ian@morland.me>
2021-12-15 13:29:53 +0000

    Handle null excerpt

--- js/src/forum/addSummaryExcerpt.tsx
+++ js/src/forum/addSummaryExcerpt.tsx
@@ -44,9 +44,11 @@ export default function addSummaryExcerpt() {
 
     // Interim fix, remove after Flarum 1.2 availability, see https://github.com/flarum/core/pull/3193
     //const content = richExcerpt ? m.trust(truncate(excerptPost.contentHtml(), excerptLength)) : truncate(excerptPost.contentPlain(), excerptLength);
+    if (!excerptPost?.contentHtml?.()) return ;
+
     const content = richExcerpt
-      ? m.trust(truncate(excerptPost.contentHtml(), excerptLength))
-      : truncate(getPlainContent(excerptPost.contentHtml()), excerptLength);
+      ? m.trust(truncate(excerptPost?.contentHtml?.(), excerptLength))
+      : truncate(getPlainContent(excerptPost?.contentHtml?.() || excerptPost?.oldContentHtml?.()), excerptLength);
 
     if (excerptPost) {
       const excerpt = <div inert>{content}</div>;
